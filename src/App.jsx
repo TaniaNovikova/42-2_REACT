332134feb9f5e324b02db397a9391503405e5ed1
TaniaNovikova <tania.novikov@web.de>
2024-11-17 09:46:05 +0100

    add Homework04 to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 // import Lesson04 from "./lessons/Lesson04/Lesson04";
 
 // import Homework02 from "./homeworks/Homework02/Homework02";
+import Homework04 from "./homeworks/Homework04/Homework04"
 
 // import Consult02 from "./consultations/Consult02/Consult02";
 
@@ -14,6 +15,7 @@ function App() {
       {/* <Lesson03/> */}
       {/* <Lesson04/> */}
       {/* <Homework02 /> */}
+      <Homework04/>
       {/* <Consult02 /> */}
     </div>
   );
