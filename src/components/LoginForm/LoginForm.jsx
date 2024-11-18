433ac2740e4aff7e778678183acfaa68b43f0c4f
TaniaNovikova <tania.novikov@web.de>
2024-11-18 10:53:58 +0100

    corrected

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,39 +3,24 @@ import Input from "../Input/Input";
 import "./styles.css";
 
 function LoginForm() {
-  const inputAttributes = [
-    {
-      id: "email-id",
-      name: "email",
-      type: "email",
-      placeholder: "Enter your email",
-      label: "Email",
-    },
-    {
-      id: "password-id",
-      name: "password",
-      type: "password",
-      placeholder: "Enter your password",
-      label: "Password",
-    },
-  ];
+  
   return (
     <form className="login-form-wrapper">
       <p className="login-form-title">Login form</p>
       <div className="inputs-container">
         <Input
-          id={inputAttributes[0].id}
-          name={inputAttributes[0].name}
-          type={inputAttributes[0].type}
-          placeholder={inputAttributes[0].placeholder}
-          label={inputAttributes[0].label}
+          id="email-id"
+          name="email"
+          type="email"
+          placeholder="Input your email"
+          label="Email"
         />
         <Input
-          id={inputAttributes[1].id}
-          name={inputAttributes[1].name}
-          type={inputAttributes[1].type}
-          placeholder={inputAttributes[1].placeholder}
-          label={inputAttributes[1].label}
+         id="password-id"
+         name="password"
+         type="password"
+         placeholder="Input your password"
+         label="Password"
         />
       </div>
       <Button name="Login" type="submit"/>
