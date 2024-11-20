c060d6205fb3e74fa288f4237f34604d0c8284c3
TaniaNovikova <tania.novikov@web.de>
2024-11-20 15:48:05 +0100

    delete unnecessary records

--- src/components/CounterExperiments/CounterExperiments.jsx
+++ src/components/CounterExperiments/CounterExperiments.jsx
@@ -1,27 +1,12 @@
-// Функция-хук useSate() возвращает массив из 2 элементов
-// - 1 элемент - это переменная сотояния
-// - 2 элемент - функция, которая меняет это состояние
-// По умолчанию count = undefined, если мы ничего не передадим в качестве аргумента в функцию useState()
-
 import "./styles.css";
 import Button from "../Button/Button";
-//Шаг 1 - импорт хука useState из react
 import { useState } from "react";
 
 function CounterExperiments() {
   console.log("Render");
 
-  //Шаг 2 - вызываем хук useState и передаем в него первоначальное состояние(initialState)
-  //делаем деструктуризацию массива из двух элементов, который возвращает хук useState
-  const [count, setCount] = useState(0);
-  // const result = useState(0);
-  // console.log(`useState(0): ${result}`);
-  // console.log(`count): ${count}`);
-  // console.log(`setCount: ${setCount}`);
+    const [count, setCount] = useState(0);
 
-
-
-  //Шаг 3 - необходимо прописать функции, которые будут менять состояние. Внутри этих функций нужно использовать setCount
   // const onPlusClick = () => {
   //   setCount((prevValue) => prevValue + 1);
   // };
@@ -32,32 +17,25 @@ function CounterExperiments() {
   //   setCount(c + 1);
   // }
 
-  // function onPlusClick() {
-  //   setCount(count + 1);
-  //   setCount(count + 1);
-  //   setCount(count + 1);
-  // }
-
-  // function onPlusClick() {
-  //   setCount(count=>(count + 1));
-  //   setCount(count=>(count + 1));
-  //   setCount(count=>(count + 1));
-
-  // }
+  function onPlusClick() {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+  }
+  
 let vania=count;
 console.log(`vania: ${vania}`);
 
-   function onPlusClick() {
-    setCount(vania=>(vania + 1));
-    console.log(`vania+1: ${vania}`);
-    console.log(setCount);
+  //  function onPlusClick() {
+  //   setCount(vania=>(vania + 1));
+  //   console.log(`vania+1: ${vania}`);
     
-    setCount(count=>(count + 5));
-    console.log(`count+5: ${count}`);
+  //   setCount(count=>(count + 5));
+  //   console.log(`count+5: ${count}`);
 
-    setCount(vasya=>(vasya + 2));
-    console.log(`vasya+2: ${count}`);
-  }
+  //   setCount(vasya=>(vasya + 2));
+  //   console.log(`vasya+2: ${count}`);
+  // }
 
   const reset = () => {
     setCount(0);
